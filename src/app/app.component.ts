6946a2ec8d394b5f7f9251ace4d141d7a4776024
omar karake <omar.karake@amalitechtraining.org>
2024-09-05 17:21:21 +0200

    ft: transforming the http call into an observable of products and implementing error handling and displaying product in the console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductServiceService } from './services/product-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-state-management-and-api-integration';
+  constructor(private productService: ProductServiceService) {
+    this.productService.init();
+  }
+  ngOnInit(): void {
+    this.productService.products$.subscribe((data) => {
+      console.log('Data initial in app component: ', data);
+    });
+  }
 }
--- src/app/services/product-service.service.ts
+++ src/app/services/product-service.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, Observable, of, retry } from 'rxjs';
+import { Product } from '../model/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductServiceService {
+  productUrl: string = 'assets/data.json'; // Ensure correct URL
+  private productSubject = new BehaviorSubject<Product[]>([]);
+  products$: Observable<Product[]> = this.productSubject.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  init(): void {
+    this.getDesserts(); // Calls the API
+  }
+
+  getDesserts() {
+    this.http
+      .get<Product[]>(this.productUrl)
+      .pipe(
+        retry(1),
+        catchError((error) => {
+          console.error('Error fetching data: ', error); // Better logging for error
+          return of([]); // Returning empty array on error
+        })
+      )
+      .subscribe((data) => this.productSubject.next(data)); // Push the fetched data to BehaviorSubject
+  }
 }
